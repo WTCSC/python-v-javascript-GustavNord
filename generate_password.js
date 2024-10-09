9788d2263da726200f783890314ad9c5f75349aa
GustavNord <gus.nordqvist@gmail.com>
2024-10-09 14:40:05 -0600

    comments added

--- generate_password.js
+++ generate_password.js
@@ -1,10 +1,15 @@
+// Creates a function named generatePassowrd to make a random password with 12 characters
 function generatePassword(length = 12) {
+    // Assigns characters to a set of characters that it can choose from
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
+    // Creates an empty string called password
     let password = '';
+    // Creates a for loop that will repeat the steps below "length" amount of times (12)
     for (let i = 0; i < length; i++) {
-      password += characters.charAt(Math.floor(Math.random() * characters.length));
+        // Picks a random character from the characters string and adds it to password string
+      password += characters.charAt(Math.random() * characters.length);
     }
     return password;  
 }
 
-module.exports = generatePassword;
+module.exports = generatePassword;  
