8c5890f483d7f26b0a546fedf29180073d4369fb
GustavNord <gus.nordqvist@gmail.com>
2024-10-09 15:05:43 -0600

    Removed a comment and fixed line 10

--- generate_password.js
+++ generate_password.js
@@ -7,8 +7,9 @@ function generatePassword(length = 12) {
     // Creates a for loop that will repeat the steps below "length" amount of times (12) then adds 1 to i until i is no longer less than length in which the for loop stops
     for (let i = 0; i < length; i++) {
         // Picks a random character from the characters string and adds it to password string
-      password += characters.charAt(Math.random() * characters.length);
+      password += characters.charAt(Math.floor(Math.random() * characters.length));
     }
+    // Returns the generated passwords
     return password;
 }
 
