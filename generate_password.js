ab80570f3e362bea7c5fd1fc3ba200fd420d208c
GustavNord <gus.nordqvist@gmail.com>
2024-10-09 13:23:05 -0600

    update

--- generate_password.js
+++ generate_password.js
@@ -1,5 +1,10 @@
 function generatePassword(length = 12) {
-    // complete me
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_.,:;<>/=';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;  
 }
 
 module.exports = generatePassword;
