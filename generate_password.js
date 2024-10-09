bf548635002be14f48d94403080b49eb511a91f6
GustavNord <gus.nordqvist@gmail.com>
2024-10-09 14:22:05 -0600

    only letters #s and _

--- generate_password.js
+++ generate_password.js
@@ -1,5 +1,5 @@
 function generatePassword(length = 12) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_.,:;<>/=';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
     let password = '';
     for (let i = 0; i < length; i++) {
       password += characters.charAt(Math.floor(Math.random() * characters.length));
